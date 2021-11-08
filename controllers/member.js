9aa50e9b59d8057d7072d9e501bb580e30c44ca6
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-11-08 07:44:36 +0000

    Update member.js

--- controllers/member.js
+++ controllers/member.js
@@ -5,11 +5,95 @@ const Error500 = require("../middleware/errors/Error500");
 
 
 
-/** GET /member/:name — returns this member */
+/** GET /:team/members/:name — returns a member by his name in a team */
 module.exports.getMemberbyName = (req, res) => {
-  Member.findOne({ name: req.params.name})
+  const {
+    teamname
+  } = req.params.team;
+  const {
+    membername
+  } = req.params.name;
+  Member.findOne({
+      team: teamname,
+      name: membername
+    })
     .then((member) => {
+      if (!member) {
+        throw new Error404('Member name is not valid');
+      }
       res.status(200).send(member);
+    })
+    .catch((err) => {
+      console.log(err);
+      throw new Error500('Member not found');
+    })
+};
+
+/**GET /:team/members - returns all members of a team */
+module.exports.getAllMembers = (req, res) => {
+  const {
+    teamname
+  } = req.params.team;
+  Member.find({
+      team: teamname,
+    })
+    .then((members) => {
+      if (!members) {
+        throw new Error404('No Members found in this team');
+      }
+      res.status(200).send(members);
+    })
+    .catch((err) => {
+      console.log(err);
+      throw new Error500('Members not found');
+    })
+};
+
+/**POST /:team/members - create a new member */
+module.exports.createMember = (req, res) => {
+  const {
+    teamname
+  } = req.params.team;
+  const {
+    membername
+  } = req.body;
+  Member.create({
+      team: teamname,
+      name: membername,
+      available: true
+    })
+    .then((member) => {
+      if (!member) {
+        throw new Error404('Member not created');
+      }
+      res.status(200).send(member);
+    })
+    .catch((err) => {
+      console.log(err);
+      throw new Error500('Member not created');
+    })
+};
+
+/** DELETE /:team/members/:name — delete a member by his name in a team */
+module.exports.deleteMemberbyName = (req, res) => {
+  const {
+    teamname
+  } = req.params.team;
+  const {
+    membername
+  } = req.params.name;
+  Member.findOneAndDelete({
+      team: teamname,
+      name: membername
+    })
+    .then((member) => {
+      if (!member) {
+        throw new Error404('Member name is not valid');
+      }
+      res.status(200).send({
+        message: "Member has been deleted",
+        data: member
+      });
     })
     .catch((err) => {
       console.log(err);
